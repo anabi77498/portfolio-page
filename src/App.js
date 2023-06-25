1b77ac231b041da929c842db50740a2e600f4f1a
Asad Nabi <asadnabi7@gmail.com>
2023-06-25 13:25:30 -0400

    initial homepage banner complete

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { NavBar } from './components/NavBar';
+import { Banner } from './components/Banner';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <header className="App-header">
+      <Banner />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
